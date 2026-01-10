a6de8cb00322973666f5b4b8dc0e5fd645122cb9
NotLebedev <notlebedev@gmail.com>
2026-01-10 21:05:32 +0300

    Add pages with individual entries

--- src/layout.rs
+++ src/layout.rs
@@ -1,4 +1,4 @@
-use printpdf::Mm;
+use printpdf::{Mm, Pt};
 
 pub const DPI: f32 = 300.0;
 pub const DPM: f32 = DPI / 25.4;
@@ -10,4 +10,13 @@ pub mod page {
 
     pub const WIDTH: Mm = Mm(210.0);
     pub const HEIGHT: Mm = Mm(297.0);
+}
+
+pub mod qr_multi {
+    use super::*;
+
+    pub const FONT_SIZE: Pt = Pt(10.0);
+    pub const QR_SIZE: Mm = Mm(50.0);
+    pub const OFFSET_X: Mm = Mm(page::WIDTH.0 / 3.0);
+    pub const OFFSET_Y: Mm = Mm(page::HEIGHT.0 / 5.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ::image::Luma;
 use printpdf::{
-    Mm, Op, PdfDocument, PdfPage, PdfSaveOptions, RawImage, RawImageData, XObjectTransform,
+    BuiltinFont, Color, Mm, Op, PdfDocument, PdfPage, PdfSaveOptions, Point, Pt, RawImage,
+    RawImageData, Rgb, TextItem, XObjectTransform,
 };
 use qrcode::QrCode;
 
@@ -14,6 +15,12 @@ fn main() {
     let mut doc = PdfDocument::new("passqr");
     let mut pages = Vec::new();
 
+    let mut data_iter = data.iter().peekable();
+
+    while data_iter.peek().is_some() {
+        pages.push(page_8_qrs(&mut doc, &mut data_iter));
+    }
+
     pages.push(full_qr_page(&mut doc, &toml_string));
 
     let pdf_bytes = doc
@@ -22,6 +29,50 @@ fn main() {
     std::fs::write("./output.pdf", pdf_bytes).unwrap();
 
     println!("Generated output.pdf");
+}
+
+fn page_8_qrs<'el>(
+    doc: &mut PdfDocument,
+    data: &mut impl Iterator<Item = (&'el String, &'el String)>,
+) -> PdfPage {
+    let mut ops = Vec::new();
+
+    for j in (0..=3).rev() {
+        'all: for i in 0..=1 {
+            let Some((key, value)) = data.next() else {
+                break 'all;
+            };
+
+            let origin_x =
+                layout::qr_multi::OFFSET_X * ((i + 1) as f32) - layout::qr_multi::QR_SIZE / 2.0;
+            let origin_y = layout::qr_multi::OFFSET_Y * ((j + 1) as f32)
+                - layout::qr_multi::QR_SIZE / 2.0
+                - into_mm(layout::qr_multi::FONT_SIZE) * 2.0;
+
+            let qr_image = generate_qr_code(value, layout::qr_multi::QR_SIZE);
+            let image_id = doc.add_image(&qr_image);
+            let transform = XObjectTransform {
+                translate_x: Some(origin_x.into_pt()),
+                translate_y: Some(origin_y.into_pt()),
+                ..Default::default()
+            };
+            ops.push(Op::UseXobject {
+                id: image_id,
+                transform,
+            });
+
+            let text_origin_x = origin_x;
+            let text_origin_y = origin_y + layout::qr_multi::QR_SIZE;
+
+            write_text(
+                &mut ops,
+                key.clone(),
+                Point::new(text_origin_x, text_origin_y),
+            );
+        }
+    }
+
+    PdfPage::new(layout::page::WIDTH, layout::page::HEIGHT, ops)
 }
 
 fn full_qr_page(doc: &mut PdfDocument, data: &str) -> PdfPage {
@@ -41,12 +92,43 @@ fn full_qr_page(doc: &mut PdfDocument, data: &str) -> PdfPage {
     PdfPage::new(layout::page::WIDTH, layout::page::HEIGHT, ops)
 }
 
+fn write_text(ops: &mut Vec<Op>, text: String, pos: Point) {
+    ops.extend([
+        Op::SaveGraphicsState,
+        Op::StartTextSection,
+        Op::SetTextCursor { pos },
+        Op::SetFontSizeBuiltinFont {
+            font: BuiltinFont::Helvetica,
+            size: layout::qr_multi::FONT_SIZE,
+        },
+        Op::SetLineHeight {
+            lh: layout::qr_multi::FONT_SIZE,
+        },
+        // Set text color to blue
+        Op::SetFillColor {
+            col: Color::Rgb(Rgb {
+                r: 0.0,
+                g: 0.0,
+                b: 0.0,
+                icc_profile: None,
+            }),
+        },
+        // Write text with the built-in font
+        Op::WriteTextBuiltinFont {
+            items: vec![TextItem::Text(text)],
+            font: BuiltinFont::Helvetica,
+        },
+        Op::EndTextSection,
+        Op::RestoreGraphicsState,
+    ]);
+}
+
 fn generate_qr_code(content: &str, size: Mm) -> RawImage {
     let code = QrCode::with_error_correction_level(content, qrcode::EcLevel::L)
         .expect("Failed to create QR code");
 
     let size = (size.0 * layout::DPM) as u32;
-    let image = code.render::<Luma<u8>>().min_dimensions(size, size).build();
+    let image = code.render::<Luma<u8>>().max_dimensions(size, size).build();
     let real_width = image.width();
     let real_height = image.height();
     RawImage {
@@ -56,4 +138,8 @@ fn generate_qr_code(content: &str, size: Mm) -> RawImage {
         data_format: printpdf::RawImageFormat::R8,
         tag: Vec::new(),
     }
+}
+
+fn into_mm(pt: Pt) -> Mm {
+    Mm(pt.0 * 0.35277)
 }
