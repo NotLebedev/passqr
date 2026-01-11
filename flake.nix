b299a190879d972b5adff8ca73566e9874eb2e2e
NotLebedev <notlebedev@gmail.com>
2026-01-11 17:15:15 +0300

    Add nix package for dynamic and static executables

--- flake.nix
+++ flake.nix
@@ -16,12 +16,43 @@
       system = "x86_64-linux";
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit system overlays; };
+      lib = pkgs.lib;
       makeRustToolchain = p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       craneLib = (crane.mkLib pkgs).overrideToolchain makeRustToolchain;
     in
     {
       devShells.${system}.default = craneLib.devShell {
       };
+
+      packages.x86_64-linux =
+        let
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              (craneLib.fileset.commonCargoSources ./.)
+              (lib.fileset.fromSource ./assets/font)
+            ];
+          };
+          strictDeps = true;
+
+          craneLibDefault = craneLib;
+          craneLibMusl = (crane.mkLib pkgs).overrideToolchain (
+            p:
+            (makeRustToolchain p).override {
+              targets = [ "x86_64-unknown-linux-musl" ];
+            }
+          );
+        in
+        {
+          default = craneLibDefault.buildPackage {
+            inherit src strictDeps;
+          };
+
+          static = craneLibMusl.buildPackage {
+            inherit src strictDeps;
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+          };
+        };
     };
 }
-
